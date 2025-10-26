d6ed7c615104e338c8b7da0b592c94b71cc2ac52
AbylayDosymbek <240569@astanait.edu.kz>
2025-10-26 15:41:17 +0500

    Update Edge.java

--- src/main/java/edu/aitu/daa/mst/Edge.java
+++ src/main/java/edu/aitu/daa/mst/Edge.java
@@ -1,10 +1,8 @@
 package edu.aitu.daa.mst;
-
 public class Edge {
     public final int u;
     public final int v;
     public final double w;
-
     public Edge(int u, int v, double w) {
         this.u = u;
         this.v = v;
